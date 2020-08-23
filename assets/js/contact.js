cfc80ae5d2e19291cd20a10d48a97a71d9493331
Radu Varga <antonio-radu@toptal.com>
2020-08-23 20:14:53 +0200

    new new

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -13,7 +13,7 @@ $("#contact-form").submit(function (e) {
 	body = body.replaceAll("\n", "<br>");
 
 	Email.send({
-		SecureToken : "a6cb31f3-8f1d-4bae-884c-c5df63e101be",
+		SecureToken : "e84d4126-5583-46ed-a3ef-b301f2c02743",
 	    To : "contact@raduvarga.com",
 	    From : "contact-form@raduvarga.com",
 	    FromName: name,
