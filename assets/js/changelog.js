0fd01ebbe5dd42350b07334e5ecf4d2d56e934e9
Radu Varga <antonio-radu@toptal.com>
2020-10-09 20:37:02 +0200

    test CORS

---
+++ assets/js/changelog.js
@@ -0,0 +1,29 @@
+(function ($) {
+
+	// var settings = {
+ //          'cache': false,
+ //          'dataType': "jsonp",
+ //          "async": true,
+ //          "crossDomain": true,
+ //          "url": "https://objects-us-east-1.dream.io/ua-midi-control/appcast.xml",
+ //          "method": "GET",
+ //          "headers": {
+ //              "accept": "application/json",
+ //              "Access-Control-Allow-Origin":"*"
+ //          }
+ //      }
+
+ //      $.ajax(settings).done(function (response) {
+ //          console.log(response);
+
+ //      });
+
+	$.ajax({
+	  url: "https://objects-us-east-1.dream.io/ua-midi-control/appcast.xml",
+	  context: document.body
+	}).done(function(obj) {
+	  
+	  dbg(obj);
+	});
+
+})(jQuery);
