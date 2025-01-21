97258279c0b021df1297e2a4774df359348132df
Antonio Radu Varga <varga.radu.antonio@gmail.com>
2025-01-21 14:23:17 +0200

    new update

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,3 +1,20 @@
+import { getFunctions, httpsCallable, connectFunctionsEmulator } from 'https://www.gstatic.com/firebasejs/11.2.0/firebase-functions.js'
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCBTuTYo2BggeSrY7ESlxTQhKT59S95yss",
+  authDomain: "ua-remote-control.firebaseapp.com",
+  projectId: "ua-remote-control",
+  storageBucket: "ua-remote-control.firebasestorage.app",
+  messagingSenderId: "1085540391736",
+  appId: "1:1085540391736:web:8717465561e8113480ffcd",
+  measurementId: "G-GZ71MLDYZB"
+};
+
+const app = initializeApp(firebaseConfig);
+const functions = getFunctions();
+// connectFunctionsEmulator(functions, "192.168.1.132", 5001);
+
 $("#contact-form").submit(function (e) {
 	e.preventDefault();
 	$("#submit").prop("disabled", true);
@@ -5,35 +22,24 @@ $("#contact-form").submit(function (e) {
 	let name = $("input[name=name]").val();
 	let email = $("input[name=email]").val();
 	let subject = $("input[name=subject]").val();
-	var body = $("textarea[name=message]").val();
+	var message = $("textarea[name=message]").val();
+	var platform = getOS();
+	let fromEmail = "contact-form@raduvarga.com";
+	let toEmail = "contact@raduvarga.com";
 
 	console.log("sending email...");
-	$(".contact-message").html("");
 
-	body += "\n\n--------------\nSent from: " + getOS(); 
+	let data = {name, email, topic: "Other", subject, message, platform, fromEmail, toEmail};
 
-	body = body.replaceAll("\n", "<br>");
-
-	// https://smtpjs.com/
-
-	Email.send({
-		 SecureToken : "819aa641-c445-4bc6-963a-1bd8bd4347dc",
-	    To : obfuscateEmail("contact", "raduvarga.com"),
-	    From : obfuscateEmail("contact-form", "raduvarga.com"),
-	    FromName: name,
-	    ReplyAddress : email,
-	    Subject : subject,
-	    Body : body
-	}).then(function(message) {
-		console.log(message);
-
-		if(message == "OK") {
-			$(".contact-message").html("Your message was succesfully sent!");
-	  	} else {
+    httpsCallable(functions, 'sendContactEmail')(data)
+      .then(function(result) {
+        console.log(result);
+        $(".contact-message").html("Your message was succesfully sent!");
+      }).catch(function (error){
+        	console.log(error);
 			$("#submit").prop("disabled", false);
-	  		$(".contact-message").html("There was an error sending your message: " + message);
-	  	}
-	});
+	  		$(".contact-message").html("There was an error sending your message: " + error.message);
+      });
 });
 
 function obfuscateEmail(user, domain) {
